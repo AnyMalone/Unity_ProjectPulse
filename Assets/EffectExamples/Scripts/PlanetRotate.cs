4a2eb5c1d25c01af3bafd30aaf150568b8c4da08
Cauthis <mattslorimer@gmail.com>
2021-11-15 18:01:31 +0000

    MattsLatest

---
+++ Assets/EffectExamples/Scripts/PlanetRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotate : MonoBehaviour
+{
+    public float Rotate = 1f;
+    void Update()
+    {
+        // Rotate around X Axis
+        transform.Rotate(Vector3.up * Rotate * Time.deltaTime);
+
+    }
+}
