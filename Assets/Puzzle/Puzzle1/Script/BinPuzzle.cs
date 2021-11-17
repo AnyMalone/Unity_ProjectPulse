c37635f2dacb2c78c98deffddd3becae11065480
AnyMalone <93198110+AnyMalone@users.noreply.github.com>
2021-11-17 14:11:04 +0000

    Input System not working

---
+++ Assets/Puzzle/Puzzle1/Script/BinPuzzle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinPuzzle : MonoBehaviour
+{
+    public bool puzzleCompleted = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (puzzleCompleted)
+        {
+            print(puzzleCompleted);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Puzzle"))
+        {
+            puzzleCompleted = true;
+        }
+    }
+}
